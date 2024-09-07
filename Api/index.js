4713210af8279fa86c97e9e4e88e86096fb3d99c
Rahul-Dev24 <rahulsingh908714@gmail.com>
2024-09-07 23:37:49 +0530

    Cors Fixed

--- Api/index.js
+++ Api/index.js
@@ -2,16 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
+import cors from "cors";
 const app = express();
-let port = process.env.PORT || 4040;
 dotenv.config();
+let port = process.env.PORT || 4040;
 app.use(express.json());
 
 mongoose
   .connect(process.env.DATABASE_ULI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("connected Successfully");
   })
@@ -19,11 +20,16 @@ mongoose
     console.log(err);
   });
 
-app.use("/api/auth/v1", authRouter);
+// For more detailed configuration, you can pass options
+app.use(
+  cors({
+    origin: "*", // Replace with your frontend URL
+    // methods: ["GET", "POST"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.get("/",(req,res)=>{
-  res.json({"message":"Server is running"})
-})
+app.use("/api/auth/v1", authRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -36,5 +42,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log("server Started in",port);
+  console.log("server Started in", port);
 });
