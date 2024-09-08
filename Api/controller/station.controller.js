aa06fedd910f0f7f5ef29c7b2b0653402e850fd7
Rahul-Dev24 <rahulsingh908714@gmail.com>
2024-09-08 23:41:25 +0530

    search functionality added

--- Api/controller/station.controller.js
+++ Api/controller/station.controller.js
@@ -10,3 +10,47 @@ export const addStation = async (req, res, next) => {
     next(errorHandler(401, "Station Failed to add."));
   }
 };
+
+export const searchStation = async (req, res, next) => {
+  try {
+    const query = req.query.query;
+    if (query) {
+      // Create a regular expression pattern
+      const regex = new RegExp(query, "i");
+
+      // Search for documents where either name or code matches the query
+      const stations = await Station.find({
+        $or: [{ name: { $regex: regex } }, { code: { $regex: regex } }],
+      });
+      res.status(201).json(stations);
+    }
+  } catch (err) {
+    next(errorHandler(401, "Something went wrong."));
+  }
+};
+
+export const getStation = async (req, res, next) => {
+  try {
+    // Extract page and limit from query parameters
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 15;
+
+    // Ensure limit is positive
+    if (limit <= 0) {
+      return res
+        .status(400)
+        .json({ message: "Limit must be a positive number" });
+    }
+
+    // Calculate skip value
+    const skip = (page - 1) * limit;
+
+    // Fetch the paginated records
+    const stations = await Station.find().skip(skip).limit(limit).exec();
+
+    // Send response
+    res.json(stations);
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
--- Api/routes/auth.route.js
+++ Api/routes/auth.route.js
@@ -1,11 +1,17 @@
 import express from "express";
 import { Login, signup } from "../controller/auth.controller.js";
-import { addStation } from "../controller/station.controller.js";
+import {
+  addStation,
+  searchStation,
+  getStation,
+} from "../controller/station.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", Login);
 router.post("/addStation", addStation);
+router.get("/searchStation", searchStation);
+router.get("/getStation", getStation);
 
 export default router;
