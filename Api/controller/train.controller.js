f3f04c8fa23596d004c4d0324185b707bed7e206
Rahul-Dev24 <149306097+Rahul-Dev24@users.noreply.github.com>
2024-12-13 19:16:20 +0530

    Asc order

--- Api/controller/train.controller.js
+++ Api/controller/train.controller.js
@@ -14,7 +14,7 @@ export const getTrainById = async (req, res, next) => {
 
 export const getTrain = async (req, res, next) => {
     try {
-        const train = await Train.find();
+        const train = await Train.find().sort({createdAt:1}).skip(skip).exec();
         res.status(201).json(train);
     } catch (err) {
         next(errorHandler(401, "Train Failed to add."));
